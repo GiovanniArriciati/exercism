80f7dbc4648fe71b08262eb95b9d9c1b73ab4685
Giovanni Arriciati <10683631@polimi.it>
2025-09-23 13:04:40 +0200

    It's weird how to find the prime factors you don't need to check if they are prime
    so usare git giuro

--- bash/prime-factors/prime_factors.sh
+++ bash/prime-factors/prime_factors.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+export primes=()
+isprime_array() {
+    local n=$1
+    for p in ${primes[@]}; do
+        if (( n % p == 0 )); then
+            return 1
+        fi
+    done
+    primes+=("$n")
+    # echo "${primes[@]}"
+    return 0
+}
+
 isprime() {
     local n=$1
     local i
@@ -10,6 +23,26 @@ isprime() {
     done
     return 0
 }
+
+main_with_primality_check() {
+    local n=$1
+    local output=()
+
+    # Handle factor 2
+    while [ $((n % 2)) -eq 0 ]; do
+        output+=("2")
+        n=$((n / 2))
+    done   
+    for ((i=3; i<=n; i+=2)); do
+        if isprime_array "$i";then
+            while (( n % i == 0 )); do
+                output+=("$i")
+                n=$((n / i))
+            done
+        fi
+    done
+    echo "${output[@]}"
+}   
 
 main() {
     local n=$1
@@ -32,4 +65,5 @@ main() {
     echo "${output[@]}"
 }
 
-main $@
+# main $@
+main_with_primality_check $@
